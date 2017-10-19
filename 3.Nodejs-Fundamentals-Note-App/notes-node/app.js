8c74f270e0aa4482ea4c80072af225f702dcec02
Carlos Garcia <chars24@gmail.com>
2017-10-18 22:53:11 -0500

    3.5 Getting input from user

--- 3.Nodejs-Fundamentals-Note-App/notes-node/app.js
+++ 3.Nodejs-Fundamentals-Note-App/notes-node/app.js
@@ -4,3 +4,21 @@ const fs = require('fs');
 const _ = require('lodash');
 
 const notes = require('./notes.js');
+
+// Con process.argv podemos pasar
+// un argumento mediante la consola
+console.log(process.argv)
+var command = process.argv[2];
+console.log('Command:', command);
+
+if (command === 'add') {
+  console.log('adding new note');
+}else if (command === 'list') {
+  console.log('listing all notes');
+}else if(command === 'read') {
+  console.log('Reading note');
+}else if(command === 'remove'){
+  console.log('Removing note');
+}else{
+  console.log('command not recognized');
+}
