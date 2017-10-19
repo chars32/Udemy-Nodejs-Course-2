5c154087ba7fb92c1072786e453954132edf79bc
Carlos Garcia <chars24@gmail.com>
2017-10-18 21:34:58 -0500

    3.4 Restarting app with nodemon

--- 3.Nodejs-Fundamentals(Note-App)/notes-node/app.js
+++ 3.Nodejs-Fundamentals(Note-App)/notes-node/app.js
@@ -1,21 +1,6 @@
 console.log('starting app.js');
 
 const fs = require('fs');
-const os = require('os');
-// Including 3rd party module(npm)
 const _ = require('lodash');
-// Using require to load our own file
-const notes = require('./notes.js')
 
-// console.log(_.isString(true));
-// console.log(_.isString('Carlos'));
-let filterArray = _.uniq(['carlos', 1, 'carlos', 1, 2, 3, 4])
-console.log(filterArray);
-
-// console.log(notes.addNote());
-
-// console.log('Result: ', notes.add(9, -2));
-
-// var user = os.userInfo();
-
-// fs.appendFileSync('greetings.txt', `Hello ${user.username}! You are ${notes.age}`);
+const notes = require('./notes.js');
--- 3.Nodejs-Fundamentals(Note-App)/notes-node/notes.js
+++ 3.Nodejs-Fundamentals(Note-App)/notes-node/notes.js
@@ -1,13 +1,6 @@
 console.log('Starting notes.js');
 
-// module is an object that exist in all our files
-// module.exports is a property(object) that we are looking for
-// because in this is all information we export to another file.
 module.exports.addNote = () => {
   console.log('addNote');
   return 'New note';
-}
-
-module.exports.add = (a, b) => {
-  return a+b
 }
