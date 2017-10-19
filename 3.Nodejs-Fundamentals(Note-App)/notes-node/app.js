03122ae47078410bac539975d5321020f59cd483
Carlos Garcia <chars24@gmail.com>
2017-10-18 21:16:15 -0500

    3.3 Using 3rd party modules

--- 3.Nodejs-Fundamentals(Note-App)/notes-node/app.js
+++ 3.Nodejs-Fundamentals(Note-App)/notes-node/app.js
@@ -2,12 +2,19 @@ console.log('starting app.js');
 
 const fs = require('fs');
 const os = require('os');
+// Including 3rd party module(npm)
+const _ = require('lodash');
 // Using require to load our own file
 const notes = require('./notes.js')
 
-console.log(notes.addNote());
+// console.log(_.isString(true));
+// console.log(_.isString('Carlos'));
+let filterArray = _.uniq(['carlos', 1, 'carlos', 1, 2, 3, 4])
+console.log(filterArray);
 
-console.log('Result: ', notes.add(9, -2));
+// console.log(notes.addNote());
+
+// console.log('Result: ', notes.add(9, -2));
 
 // var user = os.userInfo();
 
