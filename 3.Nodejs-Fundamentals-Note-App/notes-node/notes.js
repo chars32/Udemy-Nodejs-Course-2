8a55e568f3847049c2ce8f7d5effce7dd398bb51
Carlos Garcia <chars24@gmail.com>
2017-10-19 11:52:36 -0500

    3.8 Adding and saving notes

--- 3.Nodejs-Fundamentals-Note-App/notes-node/notes.js
+++ 3.Nodejs-Fundamentals-Note-App/notes-node/notes.js
@@ -1,7 +1,30 @@
 console.log('Starting notes.js');
 
+const fs = require('fs');
+
 var addNote = (title, body) => {
-  console.log('Adding note', title, body);
+  var notes = [];
+  var note = {
+    title,
+    body
+  };
+
+  // try and catch works to avoid errors if notes-data-json
+  // file doesnt exist
+  try {
+    var notesString = fs.readFileSync('notes-data.json');
+    notes = JSON.parse(notesString);
+  } catch (e) {
+
+  }
+   // Filtramos para que los titulos no se repitan
+  var duplicateNotes = notes.filter((note) => note.title === title);
+  // Verificamos que no existan titulos repetidos y
+  // de no existir se almacena en notes y se escribe el archivo.
+  if (duplicateNotes.length === 0){
+    notes.push(note);
+    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+  }
 };
 
 var getAll = () => {
