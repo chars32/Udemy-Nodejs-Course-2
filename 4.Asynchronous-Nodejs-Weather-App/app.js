e13da6a79c7e02acfba27ed26755f14383d8d9f9
Carlos Garcia <chars24@gmail.com>
2017-10-23 13:41:45 -0500

    4.4 Pretty Printing Objects

--- 4.Asynchronous-Nodejs-Weather-App/app.js
+++ 4.Asynchronous-Nodejs-Weather-App/app.js
@@ -4,8 +4,9 @@ const request = require('request');
 // Utilizamos el request para traer la info
 // mediante al api de google.
 request({
-  url: 'http://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia'
+  url: 'http://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia',
   json: true
 }, (error, response, body)=> {
-  console.log(body)
+  // Format JSON - Pretty Print Objects
+  console.log(JSON.stringify(body, undefined, 2))
 })
