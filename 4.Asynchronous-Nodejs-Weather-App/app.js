f07b53639fa52deb52509b7bcca5bfba96ea7d6b
Carlos Garcia <chars24@gmail.com>
2017-10-23 23:20:22 -0500

    4.5 Whats makes up an HTTP Request?

--- 4.Asynchronous-Nodejs-Weather-App/app.js
+++ 4.Asynchronous-Nodejs-Weather-App/app.js
@@ -7,6 +7,10 @@ request({
   url: 'http://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia',
   json: true
 }, (error, response, body)=> {
-  // Format JSON - Pretty Print Objects
-  console.log(JSON.stringify(body, undefined, 2))
+  // Traemos valores recorriendo el body del request,
+  // response nos da todo el objeto en general desde el status code,
+  // error nos muestra si hay algun error.
+  console.log(`Address: ${body.results[0].formatted_address}`);
+  console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+  console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
 })
