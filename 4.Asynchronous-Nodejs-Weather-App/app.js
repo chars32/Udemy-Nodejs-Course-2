cacf5f237a3a93a91e67670207d71a7fd8451fb9
Carlos Garcia <chars24@gmail.com>
2017-10-24 12:41:05 -0500

    4.7 Callback Errors

--- 4.Asynchronous-Nodejs-Weather-App/app.js
+++ 4.Asynchronous-Nodejs-Weather-App/app.js
@@ -1,7 +1,6 @@
 const request = require('request');
 const yargs = require('yargs');
 
-// empezamos constante yargs
 const argv = yargs
   .options({
     a: {
@@ -15,18 +14,24 @@ const argv = yargs
   .alias('help', 'h')
   .argv;
 
-// variable que recibe el valor que pasamos por consola
-// y lo formatea para que los espacios en blanco se 
-// llenen con %
 var encodedAddress = encodeURIComponent(argv.address);
 
 request({
-  // pasamos dinamicamente el valor de la direccion
-  // deseada para buscar.
   url: `http://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
   json: true
 }, (error, response, body)=> {
-  console.log(`Address: ${body.results[0].formatted_address}`);
-  console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-  console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
-})
+  // Manejando los errors
+  if (error) {
+    // si no se puede conectar a los servidores por algun detalle
+    // en el URL.
+    console.log('Unable to conncect to Google servers.');
+  } else if (body.status === 'ZERO_RESULTS'){
+    // si la encodedAddress pasada, devuelve el status ZERO_RESULTS
+    console.log('Unable to find that address.');
+  } else if (body.status === 'OK'){
+    // de otra forma imprima la siguiente info.
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+  }
+});
