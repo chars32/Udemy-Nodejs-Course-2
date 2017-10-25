4b5e623349bd84577e8845f9ae7f7c4629eee106
Carlos Garcia <chars24@gmail.com>
2017-10-24 22:56:48 -0500

    4.11 Intro to ES6 Promises

---
+++ 4.Asynchronous-Nodejs-Weather-App/playground/promise.js
@@ -0,0 +1,13 @@
+// Ejemplo de promise
+var somePromise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve('Hey. It worked!');
+    // reject('Unable to fullfill promise');
+  }, 2500);
+});
+
+somePromise.then((message) => {
+  console.log('Success: ', message);
+}, (errorMessage) => {
+  console.log('Error: ', errorMessage);
+});
