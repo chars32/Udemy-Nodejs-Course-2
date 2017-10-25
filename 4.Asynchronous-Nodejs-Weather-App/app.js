7ba52a0af2be8dfdc5aef565d1fc59a983d1ec8d
Carlos Garcia <chars24@gmail.com>
2017-10-24 21:38:42 -0500

    4.9 Wiring Up Weather Search

--- 4.Asynchronous-Nodejs-Weather-App/app.js
+++ 4.Asynchronous-Nodejs-Weather-App/app.js
@@ -1,24 +1,43 @@
-const yargs = require('yargs');
-const geocode = require('./geocode/geocode')
+// const yargs = require('yargs');
+// const geocode = require('./geocode/geocode')
 
-const argv = yargs
-  .options({
-    a: {
-      demand: true,
-      alias: 'address',
-      describe: 'Address to fetch weather for',
-      string: true
-    }
-  })
-  .help()
-  .alias('help', 'h')
-  .argv;
-// Refactorizamos le codigo, hicimos otro archivo y le pasamos dos
-// argumentos, el primero la address y segundo el callback
-geocode.geocodeAddress(argv.address, (errorMessage, results) => {
-  if (errorMessage) {
-    console.log(errorMessage);
-  } else {
-    console.log(JSON.stringify(results, undefined, 2));
+// const argv = yargs
+//   .options({
+//     a: {
+//       demand: true,
+//       alias: 'address',
+//       describe: 'Address to fetch weather for',
+//       string: true
+//     }
+//   })
+//   .help()
+//   .alias('help', 'h')
+//   .argv;
+
+// geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+//   if (errorMessage) {
+//     console.log(errorMessage);
+//   } else {
+//     console.log(JSON.stringify(results, undefined, 2));
+//   }
+// });
+
+// key: 5c6e0a376369780a3ec70e45f172764e
+
+// latitude: 18.0047349
+// longitude: -92.9391078
+
+const request = require('request');
+// request a forecast.io
+request({
+  url: 'https://api.darksky.net/forecast/5c6e0a376369780a3ec70e45f172764e/18.0047349,-92.9391078',
+  json: true
+}, (error, response, body) => {
+  if (error) {
+    console.log('Unable to connect to Forecast.io server')
+  } else if (response.statusCode === 400) {
+    console.log('Unable to fetch weather')
+  } else if (!error && response.statusCode === 200) {
+    console.log(body.currently.temperature)
   }
 });
