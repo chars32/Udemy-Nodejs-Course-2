fda4404801c057ae21dfa357292cf7f773234f9e
Carlos Garcia <chars24@gmail.com>
2017-10-27 22:45:58 -0500

    5.5 Express Middleware

--- 5.Web-Servers-and-Application-Deployment/server.js
+++ 5.Web-Servers-and-Application-Deployment/server.js
@@ -1,17 +1,38 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 var app = express();
-// registerPartials sirve para declarar donde estaran
-// los fragmentos de archivos hbs que se repiten en 
-// diferentes templates
+
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
+// Con app.use declaramos un middleware, le pasamos 3
+// argumentos (req, res, next), los dos primeros son 
+// ya conocidos, pero el 3ero (next) sirve para avanzar
+// al siguiente middleware, en caso que exista mas de uno 
+// como en el siguiente ejemplo.
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`
+  console.log(log)
+  
+  fs.appendFile('server.log', log+'\n', (err) => {
+    if (err) {
+      console.log('Unable to append to server.log')
+    }
+  })
+  next();
+});
+
+app.use((req, res, next) => {
+  res.render('maintance.hbs');
+});
+// OJO --- los middlewares se ejecutan segun su aparicion
+// en el codigo, es por eso que este lo bajamos para que 
+// cuando ingrese a una direccion con terminacion .html
+// tambien se vea afectado por los dos de arriba.
 app.use(express.static(__dirname + '/public'));
 
-// registerHelper lo utilizamos para agregar funciones
-// que pueden aceptar argumentos(screamIt) o no y ayudan a devolver
-// un valor.
 hbs.registerHelper('getCurrentYear', () => {
   return new Date().getFullYear();
 });
