4e3b24a8263ba3b8814ee4f81be66f35b0eae5f9
Carlos Garcia <chars24@gmail.com>
2017-10-30 14:40:47 -0600

    6.3 Using an Assertion Library

--- 6.Testing-Your-Applications/utils/utils.test.js
+++ 6.Testing-Your-Applications/utils/utils.test.js
@@ -1,21 +1,47 @@
-// Este archivo (utils.test.js) se crea para que cuando le digamos a
-// mocha que haga las pruebas, aqui sea dondelas busque. Esto lo 
-// configuramos en el package.json, en scripts.
+const expect = require('expect')
 
 const utils = require('./utils');
-// No necesitamos importar mocha. Con poner it(), se llama.
 it('should add two numbers', () => {
   var res = utils.add(33, 11);
-  
-  if (res !== 44) {
-    throw new Error(`Expected 44, but got ${res}`);
-  }
+
+  //Usamos expect en lugar de hacer la comparacion
+  //nostros mismos
+  expect(res).toBe(44).toBeA('number');  
 });
 
 it('should square the number', () => {
   var res = utils.square(3);
 
-  if ( res !== 9) {
-    throw new Error(`Expected 9, but got ${res}`);
-  }
+  expect(res).toBe(9).toBeA('number');
 });
+
+it('should set firstName and lastName', () => {
+  var user = {location: 'Philadelphia', age: 25};
+  var res = utils.setName(user, 'Andrew Mead');
+
+  // console.log(res)
+  // console.log(user)
+
+  expect(res).toInclude({
+    firstName: 'Andrew',
+    lastName: 'Mead'
+  })
+});
+
+// it('should expect some values', () => {
+  // toBe y toNotBe son buenos para comparar numeros
+  // cadenas de texto, etx
+  // expect(12).toNotBe(14)
+
+  // pero para comparar objetos usamos toEqual y toNotEqual
+  // expect({name: 'andrew'}).toNotEqual({name: 'Andrew'});
+
+  // expect([1,2,3,4]).toExclude(0);
+  // expect({
+  //   name: 'Andrew',
+  //   age: 25,
+  //   location: 'Philadelphia'
+  // }).toInclude({
+  //   age: 23
+  // })
+// });
