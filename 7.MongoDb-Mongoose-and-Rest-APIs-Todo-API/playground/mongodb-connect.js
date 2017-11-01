d1dd857ddf11255160d108a23fc0b809f33bae15
Carlos Garcia <chars24@gmail.com>
2017-10-31 23:16:59 -0600

    7.2 The ObjectId

--- 7.MongoDb-Mongoose-and-Rest-APIs-Todo-API/playground/mongodb-connect.js
+++ 7.MongoDb-Mongoose-and-Rest-APIs-Todo-API/playground/mongodb-connect.js
@@ -1,13 +1,17 @@
-const MongoClient = require('mongodb').MongoClient;
-// Hacemos conexion del mongoclient y le pasamos la direccion(local)
-// y el nombre de la bd y un callback como segundo parametro
+// const MongoClient = require('mongodb').MongoClient;
+// Haremos lo mismo de arriba pero con destructuring assignment
+const {MongoClient, ObjectID} = require('mongodb');
+
+// Para esto sirve ObjectID
+var obj = new ObjectID();
+console.log(obj);
+
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   if(err){
     return console.log('Unable to connect to MondoDB server');
   }
   console.log('Connected to MondoDB server');
-  // Agregamos una coleccion y le insertamos datos,
-  // pasamos un callback como segundo argumento.
+
   // db.collection('Todos').insertOne({
   //   text: 'Something to do',
   //   completed: false,
@@ -19,17 +23,20 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   //   console.log(JSON.stringify(result.ops, undefined, 2));
   // });
 
-  db.collection('Users').insertOne({
-    name: 'Carlos',
-    age: 37,
-    location: 'Villahermosa'
-  }, (err, result) => {
-    if(err){
-      return console.log('Unable to insert user');
-    }
+  // db.collection('Users').insertOne({
+  //   // Podemos declarar nuestro propio _id
+  //   // _id: 123,
+  //   name: 'Carlos',
+  //   age: 37,
+  //   location: 'Villahermosa'
+  // }, (err, result) => {
+  //   if(err){
+  //     return console.log('Unable to insert user');
+  //   }
 
-    console.log(JSON.stringify(result.ops, undefined, 2));
-  });
-  // Cerramos base de datos
+  //   // console.log(JSON.stringify(result.ops, undefined, 2));
+  //   console.log(result.ops[0]._id.getTimestamp());
+  // });
+
   db.close();
 });
