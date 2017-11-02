4280968efcbe4413576751ef47c8123cef55f188
Carlos Garcia <chars24@gmail.com>
2017-11-01 22:47:14 -0600

    7.8 Resource Creation Endpoint - POST/todos

--- 7.MongoDb-Mongoose-and-Rest-APIs-Todo-API/server/server.js
+++ 7.MongoDb-Mongoose-and-Rest-APIs-Todo-API/server/server.js
@@ -1,54 +1,29 @@
-var mongoose = require('mongoose');
+// El archivo se dividio en varios otros.
+var express = require('express');
+// BodyParser manda JSON al servidor.
+var bodyParser = require('body-parser');
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp', {
-  useMongoClient: true
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
+
+var app = express();
+// Middleware bodyparser
+app.use(bodyParser.json());
+// Ruta para hacer POST
+app.post('/todos', (req, res) => {
+  // Aqui utilizamos el model Todo de todo.js
+  var todo = new Todo({
+    text: req.body.text
+  });
+  // Guardamos
+  todo.save().then((doc) => {
+    res.send(doc);
+  }, (e) => {
+    res.status(400).send(e);
+  });
 });
 
-var Todo = mongoose.model('Todo', {
-  text: {
-    // Aplicamos los validadores, type y defaults.
-    type: String,
-    required: true,
-    minlength: 1,
-    trim: true
-  },
-  completed: {
-    type: Boolean,
-    default: false
-  },
-  completedAt: {
-    type: Number,
-    default: null
-  }
-});
-
-// var otherTodo = new Todo({
-//   text: '  Edit this video  '
-// });
-
-// otherTodo.save().then((doc) => {
-//   console.log(JSON.stringify(doc, undefined, 2));
-// }, (e) => {
-//   console.log('Unable to save todo', e);
-// });
-
-var User = mongoose.model('User', {
-  email: {
-    // Aplicamos validadores, types y defaults
-    type: String,
-    required: true,
-    trim: true,
-    minlength: 1
-  }
-});
-
-var user = new User({
-  email: '   charsalgo@gmail.com  '
-});
-
-user.save().then((doc) => {
-  console.log('Email saved!!!', doc);
-}, (e) => {
-  console.log('Unable to save email', e);
+app.listen(3000, () => {
+  console.log('Started on port 3000');
 });
