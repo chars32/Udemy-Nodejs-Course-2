c73ba84ef0adafe23aaf64216ecc90be355643a8
Carlos Garcia <chars24@gmail.com>
2017-11-01 21:31:24 -0600

    7.7 Validators, Types and Defaults

--- 7.MongoDb-Mongoose-and-Rest-APIs-Todo-API/server/server.js
+++ 7.MongoDb-Mongoose-and-Rest-APIs-Todo-API/server/server.js
@@ -1,44 +1,54 @@
 var mongoose = require('mongoose');
 
-// mongoose funciona con callbacks por lo cual hacemos
-// este truco para que funcione con promesas
 mongoose.Promise = global.Promise;
-// nos conectamos a la bd con mongoose
 mongoose.connect('mongodb://localhost:27017/TodoApp', {
   useMongoClient: true
 });
-// declaramos nuestro modelo
+
 var Todo = mongoose.model('Todo', {
   text: {
-    type: String
+    // Aplicamos los validadores, type y defaults.
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
   },
   completed: {
-    type: Boolean
+    type: Boolean,
+    default: false
   },
   completedAt: {
-    type: Number
+    type: Number,
+    default: null
   }
 });
-// aqui pasamos un nuevo document a la coleccion
-// cumpliendo con el modelo declarado arriba
-var newTodo = new Todo({
-  text: 'Cook dinner'
-});
-// asi la guardamos en la bd, usando promesas
-newTodo.save().then((doc) => {
-  console.log('Save todo', doc);
-}, (e) => {
-  console.log('Unable to save todo', e);
+
+// var otherTodo = new Todo({
+//   text: '  Edit this video  '
+// });
+
+// otherTodo.save().then((doc) => {
+//   console.log(JSON.stringify(doc, undefined, 2));
+// }, (e) => {
+//   console.log('Unable to save todo', e);
+// });
+
+var User = mongoose.model('User', {
+  email: {
+    // Aplicamos validadores, types y defaults
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 1
+  }
 });
 
-var otherTodo = new Todo({
-  text: 'Learning MongoDB',
-  completed: true,
-  completedAt: 12345
+var user = new User({
+  email: '   charsalgo@gmail.com  '
 });
 
-otherTodo.save().then((doc) => {
-  console.log(JSON.stringify(doc, undefined, 2));
+user.save().then((doc) => {
+  console.log('Email saved!!!', doc);
 }, (e) => {
-  console.log('Unable to save todo', e);
+  console.log('Unable to save email', e);
 });
