2ac1d1c26e37d5d49b81086999a1ef301dafcbed
Carlos Garcia <chars24@gmail.com>
2017-11-01 23:24:10 -0600

    7.9 Testing POST/todos

--- 7.MongoDb-Mongoose-and-Rest-APIs-Todo-API/server/server.js
+++ 7.MongoDb-Mongoose-and-Rest-APIs-Todo-API/server/server.js
@@ -1,6 +1,4 @@
-// El archivo se dividio en varios otros.
 var express = require('express');
-// BodyParser manda JSON al servidor.
 var bodyParser = require('body-parser');
 
 var {mongoose} = require('./db/mongoose');
@@ -8,15 +6,14 @@ var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
-// Middleware bodyparser
+
 app.use(bodyParser.json());
-// Ruta para hacer POST
+
 app.post('/todos', (req, res) => {
-  // Aqui utilizamos el model Todo de todo.js
   var todo = new Todo({
     text: req.body.text
   });
-  // Guardamos
+
   todo.save().then((doc) => {
     res.send(doc);
   }, (e) => {
@@ -27,3 +24,5 @@ app.post('/todos', (req, res) => {
 app.listen(3000, () => {
   console.log('Started on port 3000');
 });
+
+module.exports = {app};
