337c5439e34a9a83e15c648b5b54c727e81cfb23
Carlos Garcia <chars24@gmail.com>
2017-11-19 18:43:37 -0600

    10.4 Handling Errors and Awaiting Async Function

--- 10.Async-Await-Just-Added/currency-convert.js
+++ 10.Async-Await-Just-Added/currency-convert.js
@@ -2,18 +2,34 @@
 // 23 USD is worth 28 CAD. You can spend these in the following countries:
 
 const axios = require('axios');
+// cambiamos las dos funciones getExchangeRate y getCountries y usamos
+// async await para que se vuelvan asincronas 
+const getExchangeRate = async (from, to) => {
+  // Utilizamos try/catch para manejar los errors
+  try {
+    const response = await axios.get(`https://api.fixer.io/latest?base=${from}`);
+    const rate = response.data.rates[to];
 
-const getExchangeRate = (from, to) => {
-  return axios.get(`https://api.fixer.io/latest?base=${from}`).then((response) => {
-    return response.data.rates[to];
-  });
-}
+    if (rate) {
+      return rate;
+    }else {
+      throw new Error(`Unable to get exhange rate for ${from} and ${to}`);
+    }
 
-const getCountries = (currencyCode) => {
-  return axios.get(`https://restcountries.eu/rest/v2/currency/${currencyCode}`).then ((response) => {
-    return response.data.map((country) => country.name);
-  })
+  }catch (e) {
+    throw new Error(`Unable to get exhange rate for ${from} and ${to}`)
+  }
 };
+
+const getCountries = async (currencyCode) => {
+  try {
+    const response = await axios.get(`https://restcountries.eu/rest/v2/currency/${currencyCode}`)
+    return response.data.map((country) => country.name);
+  } catch (e) {
+    throw new Error(`Unable to get countries that use ${currencyCode}`)
+  }
+};
+
 // Promises mode
 const convertCurrency = (from, to, amount) => {
   let countries;
@@ -31,11 +47,11 @@ const getConvertCurrency = async (from, to, amount) => {
   const listCountries = await getCountries(to);
   const exchangeRate = await getExchangeRate(from, to);
   const exchangedAmount = amount * exchangeRate
-
+  
   return `${amount} ${from} is worth ${exchangedAmount} ${to}. ${to} can be used in the following countries: ${listCountries.join(', ')}`;
 }
 
 
-getConvertCurrency('MXN', 'USD', 1000).then((status) => {
+getConvertCurrency('USD', 'EUR', 1000).then((status) => {
   console.log(status)
-})
+}).catch((e) => console.log(e.message));
